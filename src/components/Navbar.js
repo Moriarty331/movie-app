5f9ffbad0c7aa0baa035ca75a3b4f1068ddbc88b
Mark <nunezchristian77@gmail.com>
2023-10-18 19:37:48 +0800

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+
+
+export const Navbar = () =>{
+    return(
+        <div className="navbar">
+            <ul className="navbar-nav">
+                
+            </ul>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+
+import '../styles/home.css'
+
+export const Home = () =>
+{
+    return (
+        <h2>This is the homepage</h2>
+    )
+}
